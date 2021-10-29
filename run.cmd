77eb3ba935c1ee6be80b4c00f2adeccdbd657e4d
Dirk Arnez <smalldirkalex@gmail.com>
2021-10-29 22:55:53 +0800

    - update run.cmd

--- run.cmd
+++ run.cmd
@@ -1,5 +1,15 @@
 @echo off
 set /p url="Enter input file name / full path: "
 set /p filename="Enter output file name / full path: "
-ffmpeg -i "%url%" -c:v libx264 -pix_fmt yuv420p -c:a aac "%filename%"
+ffmpeg -i "%url%" -c:v libx264 -pix_fmt yuvj420p -c:a aac "%filename%"
 pause
+
+@echo off
+set /p url="Enter input file name / full path: "
+set /p filename="Enter output file name / full path: "
+set /p start="Start: "
+set /p end="End: "
+ffmpeg -i "%url%" -ss %start% -to %end% -c:v libx264 -pix_fmt yuvj420p -crf 18 -c:a aac "%filename%"
+pause
+
+https://ottverse.com/trim-cut-video-using-start-endtime-reencoding-ffmpeg/#-t_parameter
