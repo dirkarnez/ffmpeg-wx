fb906c101a9c04ae94071aa1af9bf9d5dab166a2
Dirk Arnez <smalldirkalex@gmail.com>
2021-11-24 20:37:36 +0800

    - update run.cmd

--- run.cmd
+++ run.cmd
@@ -10,6 +10,7 @@ set /p filename="Enter output file name / full path: "
 set /p start="Start: "
 set /p end="End: "
 ffmpeg -i "%url%" -ss %start% -to %end% -c:v libx264 -crf 18 -c:a aac "%filename%"
+ffmpeg -i "%url%" -ss %start% -to %end% -filter:v "crop=1090:625:100:100" -c:v libx264 -crf 18 -c:a aac "%filename%"
 pause
-
+ 
 https://ottverse.com/trim-cut-video-using-start-endtime-reencoding-ffmpeg/#-t_parameter
